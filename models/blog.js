ca4a6df5b83c3ed7345915ecfb84e4f032de44a9
runtee <ekwo.chinonso.01@gmail.com>
2022-07-14 09:57:46 +0100

    first commit

---
+++ models/blog.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+
+const BlogSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    comments: [
+		{
+			body: String,
+			username: String,
+			createdAt: String,
+			userId: Schema.Types.ObjectId
+		}
+	],
+    creator:{
+        type:Schema.Types.ObjectId,
+        required:true
+    },
+    created:{
+        type:String,
+        required:true
+    }
+})
+const Blog = mongoose.model('Blog', BlogSchema);
+module.exports=Blog

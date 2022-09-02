7876826f0bd3d14cc83187908e9a641058e3940a
runtee <ekwo.chinonso.01@gmail.com>
2022-09-02 18:41:01 +0100

    starting

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Navbar from './components/Navbar';
+import { height } from '@mui/system';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ bgcolor: '#eff0f3', height: '100vh' }} >
+<Navbar/>
+    </Box>
   );
-}
-
+  }
 export default App;
